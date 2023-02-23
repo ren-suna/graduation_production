30525d2051fed628e8d3793894a3f30ccb1a6a7d
ren-suna <hbunny206-sunabaco@yahoo.co.jp>
2023-02-23 19:50:09 +0900

    ren

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from flask import redirect
 import random
 import sqlite3
 
+
 app = Flask(__name__)
-
-
-
-
-
-
+@app.route("/test")
+# /にアクセスしたら
+def test():
+    return render_template("base.html")
+# index.htmlを使って　HTMLを生成
 
 if __name__ == "__main__":
     app.run(debug=True,port=8080)
