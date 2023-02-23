617d736d58b555af7c200c47c68af1b886ef3d31
HirokiOhi <house.going.874@gmail.com>
2023-02-23 22:12:53 +0900

    test

--- static/canvas.js
+++ static/canvas.js
@@ -1,4 +1,13 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 100, 100);
+
+function insert_zyouge(){
+    var img = new Image();
+    img.src = "static\img\上下矢印.png";
+    img.onload = () => {
+        ctx.drawImage(img, 0, 0);
+    };
+}
+
+
+
