79082e612a2518f5b0079cbf8fe49467f30c89c8
ren-suna <hbunny206-sunabaco@yahoo.co.jp>
2023-02-24 22:21:52 +0900

    2.24

--- app.py
+++ app.py
@@ -6,11 +6,24 @@ from flask import request
 
 app=Flask(__name__)
 
+@app.route("/left1")
+def left1():
+    return render_template("R.main_left1.html")
+
+@app.route("/left2")
+def left2():
+    return render_template("R.main_left2.html")
+
+@app.route("/right")
+def right():
+    return render_template("R.main_right.html")
+
 #flaskからHTMLを生成
 @app.route("/")
 def test():
-    print(test)
     return render_template("R.main.html")
+
+
 
 
     #Flask実行（ターミナルでflask実行できなかったので）
