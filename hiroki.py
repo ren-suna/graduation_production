b888d06c89dbc3ff33e0cb9bd22f057baf99b621
HirokiOhi <house.going.874@gmail.com>
2023-02-25 19:41:07 +0900

    根性

---
+++ hiroki.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect
+
+import sqlite3
+# CGIモジュールのインポート
+from flask import request
+
+app=Flask(__name__)
+
+#flaskからHTMLを生成
+@app.route("/")
+def test():
+    print(test)
+    return render_template("H_madorizuadd.html")
+
+    #Flask実行（ターミナルでflask実行できなかったので）
+if __name__ =="__main__":
+    app.run(debug=True) #エラーはいてね！（サーバーを毎回落とさなくていい）
